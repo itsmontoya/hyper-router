dd2cdbf32e5288dbde717af26879ac23696a5b4e
Josh Montoya <josh@montoya.io>
2020-07-18 10:27:17 -0700

    Add subFn

--- utils.js
+++ utils.js
@@ -1,1 +1,2 @@
-export const wrap = (subFn, action) => (...args) => [action, subFn(args)]
+export const wrap = (subFn, action) => (...args) => [action, subFn(args)];
+export const subFn = ([action]) => ({ action });
