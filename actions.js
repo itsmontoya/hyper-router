ff0d1e3304d871c413ab31858edd9b3d104b37f2
Josh Montoya <josh@montoya.io>
2020-07-24 13:00:24 -0700

    small clean-up

--- actions.js
+++ actions.js
@@ -2,6 +2,5 @@ export const routeChanged = (state, match) => ({
 	...state,
 	currentRoute: match.route,
 	currentParams: match.params
-})
+});
 
-export const linkClicked = (state, location) => [state, pushUrl(location.pathname)]
