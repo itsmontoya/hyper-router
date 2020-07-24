e0c92a27f79b62ab24a4c42f6582ee997c4c20a6
Josh Montoya <josh@montoya.io>
2020-07-24 12:58:30 -0700

    add effect, sub, and deferredAction

--- utils.js
+++ utils.js
@@ -1,2 +1,11 @@
 export const wrap = (subFn, action) => (...args) => [action, subFn(args)];
 export const subFn = ([action]) => ({ action });
+
+export const effect = (state, action, props) => [
+	state,
+	sub(action, props),
+];
+
+export const sub = (action, props) => [(dispatch, props) => dispatch(action, props), props];
+
+export const deferredAction = (action, props) => [action, props];
